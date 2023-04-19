03d6c7f62ef725fa97e09211a34fc55118a8a4bf
Dejatori <deivitsc@gmail.com>
2023-04-18 19:36:55 -0500

    Actualización

--- db/clinica_oftalmologica.sql
+++ db/clinica_oftalmologica.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE IF NOT EXISTS `clinica_oftalmologica` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `clinica_oftalmologica` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE `clinica_oftalmologica`;
---
+++ db/periodo.sql
@@ -0,0 +1,6 @@
+CREATE TABLE periodo (
+  id_periodo int(11) NOT NULL AUTO_INCREMENT,
+  fecha_inicio date NOT NULL,
+  fecha_fin date NOT NULL,
+  PRIMARY KEY (id_periodo)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
